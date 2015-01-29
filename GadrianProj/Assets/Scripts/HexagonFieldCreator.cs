b6e380962f595ed6dd9b8ff93f5e296aa8ac55d6
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2015-01-28 23:55:31 -0500

    Test hexagon field

---
+++ GadrianProj/Assets/Scripts/HexagonFieldCreator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexagonFieldCreator : MonoBehaviour
+{
+	public GameObject hexagonPrefab;
+	public int multiplier = 4;
+	public bool isSeparated = false;
+	public int separation = 0;
+
+	void Start ()
+	{
+		
+	}
+}
