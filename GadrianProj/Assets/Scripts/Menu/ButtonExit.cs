884666a22c9769a248b22459bdd1d1c2b85d763b
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2015-05-27 09:40:40 -0500

    Menu EndGameNotification

---
+++ GadrianProj/Assets/Scripts/Menu/ButtonExit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonExit : MonoBehaviour
+{
+    public void ExitGame ()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPaused = true;
+#endif
+#if UNITY_ANDROID
+        Application.Quit();
+#endif
+    }
+}
---
+++ GadrianProj/Assets/Scripts/Menu/ButtonRepeat.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonRepeat : MonoBehaviour
+{
+    public void LoadSameLevel ()
+    {
+        Application.LoadLevel( Application.loadedLevel );
+    }
+}
