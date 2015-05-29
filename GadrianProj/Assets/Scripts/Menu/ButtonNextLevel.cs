9419d209db4278e907d425f865961d7018f6b9f5
Jhon Robinson Contreras Castillo <jrcontrerasca@gmail.com>
2015-05-28 19:55:59 -0500

    Animación EndGame y buttonNextLevel fix

--- GadrianProj/Assets/Scripts/Menu/ButtonNextLevel.cs
+++ GadrianProj/Assets/Scripts/Menu/ButtonNextLevel.cs
@@ -1,8 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
+[RequireComponent(typeof(Button))]
 public class ButtonNextLevel : MonoBehaviour
 {
+    private Button buttonNextLevel;
+
+    public void Awake ()
+    {
+        buttonNextLevel = GetComponent<Button>();
+    }
+
+    public void Start ()
+    {
+        if ( !IsThereANextLevel() )
+        {
+            buttonNextLevel.interactable = false;
+        }
+    }
+
+    private bool IsThereANextLevel ()
+    {
+        int levelToLoad = PlayerPrefs.GetInt( Strings.LevelToLoad, 0 );
+        string fileName = string.Format( Strings.GenericLevelName, ++levelToLoad );
+        if ( Resources.Load<Level>( Strings.LevelPath + fileName ) != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    
     public void LoadNextLevel ()
     {
         int levelToLoad = PlayerPrefs.GetInt( Strings.LevelToLoad, 0 );
